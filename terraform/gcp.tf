f75f240cf09f0205234b8e692b8e9569e8f78e44
Matt Black <github@mafro.net>
2018-09-05 14:19:22 +1000

    Enable GCP secrets engine for vault

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -186,6 +186,7 @@ resource "google_container_cluster" "vault" {
       "https://www.googleapis.com/auth/devstorage.read_write",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/iam",
     ]
 
     tags = ["vault"]
