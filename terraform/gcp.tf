54ab20469adf904c070860776b50c4bf108f5335
Matt Black <github@mafro.net>
2018-09-22 18:22:07 +1000

    Deploy Consul cluster

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -47,62 +47,6 @@ resource "google_project_service" "service" {
   disable_on_destroy = false
 }
 
-# Create the storage bucket
-resource "google_storage_bucket" "vault" {
-  name          = "${google_project.vault.project_id}-vault-storage"
-  project       = "${google_project.vault.project_id}"
-  force_destroy = true
-  storage_class = "MULTI_REGIONAL"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-
-    condition {
-      num_newer_versions = 3
-    }
-  }
-
-  depends_on = ["google_project_service.service"]
-}
-
-# Grant service account access to the storage bucket
-resource "google_storage_bucket_iam_member" "vault-server" {
-  count  = "${length(var.storage_bucket_roles)}"
-  bucket = "${google_storage_bucket.vault.name}"
-  role   = "${element(var.storage_bucket_roles, count.index)}"
-  member = "serviceAccount:${google_service_account.vault-server.email}"
-}
-
-# Create the KMS key ring
-resource "google_kms_key_ring" "vault" {
-  name     = "vault"
-  location = "${var.region}"
-  project  = "${google_project.vault.project_id}"
-
-  depends_on = ["google_project_service.service"]
-}
-
-# Create the crypto key for encrypting init keys
-resource "google_kms_crypto_key" "vault-init" {
-  name            = "vault-init"
-  key_ring        = "${google_kms_key_ring.vault.id}"
-  rotation_period = "604800s"
-}
-
-# Grant service account access to the key
-resource "google_kms_crypto_key_iam_member" "vault-init" {
-  count         = "${length(var.kms_crypto_key_roles)}"
-  crypto_key_id = "${google_kms_crypto_key.vault-init.id}"
-  role          = "${element(var.kms_crypto_key_roles, count.index)}"
-  member        = "serviceAccount:${google_service_account.vault-server.email}"
-}
-
 resource "google_compute_network" "shared_vpc" {
   name                    = "${random_id.random.hex}-vpc"
   auto_create_subnetworks = "false"
@@ -196,8 +140,6 @@ resource "google_container_cluster" "vault" {
   lifecycle = {
     ignore_changes = ["node_pool"]
   }
-
-  depends_on = ["google_project_service.service"]
 }
 
 resource "google_container_node_pool" "vault" {
@@ -240,7 +182,7 @@ resource "google_container_node_pool" "vault" {
   ]
 }
 
-# Provision Global Static IP
+# Provision an external static IP for Vault
 resource "google_compute_address" "vault" {
   name    = "vault-lb"
   region  = "${var.region}"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,27 +33,9 @@ variable "project_services" {
   type = "list"
 
   default = [
-    "cloudkms.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "container.googleapis.com",
     "iam.googleapis.com",
-  ]
-}
-
-variable "storage_bucket_roles" {
-  type = "list"
-
-  default = [
-    "roles/storage.legacyBucketReader",
-    "roles/storage.objectAdmin",
-  ]
-}
-
-variable "kms_crypto_key_roles" {
-  type = "list"
-
-  default = [
-    "roles/cloudkms.cryptoKeyEncrypterDecrypter",
   ]
 }
 
