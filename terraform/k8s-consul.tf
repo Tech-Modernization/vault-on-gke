c43e5cc7a53469568cdf5d96a24aefecd7133b5e
Matt Black <github@mafro.net>
2018-09-25 07:55:18 +1000

    Switch to Consul Enterprise

--- terraform/k8s-consul.tf
+++ terraform/k8s-consul.tf
@@ -6,10 +6,20 @@ resource "null_resource" "gcr-consul" {
 
   provisioner "local-exec" {
     command = <<EOF
-docker pull "consul:1.2.3"
-docker tag "consul:1.2.3" "gcr.io/${google_project.vault.project_id}/consul:1.2.3"
-docker push "gcr.io/${google_project.vault.project_id}/consul:1.2.3"
+docker tag "consul-enterprise:1.2.3" "gcr.io/${google_project.vault.project_id}/consul-enterprise:1.2.3"
+docker push "gcr.io/${google_project.vault.project_id}/consul-enterprise:1.2.3"
 EOF
+  }
+}
+
+# Write Consul license to kubernetes secrets
+resource "kubernetes_secret" "consul-license" {
+  metadata {
+    name = "consul-license"
+  }
+
+  data {
+    "consul.license" = "${file(var.consul_license_path)}"
   }
 }
 
@@ -41,9 +51,43 @@ EOF
   }
 
   # GKE cluster must be ready
+  # Consul license must be available in kubernetes
   # Consul image must be in GCR
   depends_on = [
     "google_container_node_pool.vault",
     "null_resource.gcr-consul",
+    "kubernetes_secret.consul-license",
   ]
+}
+
+# Wait for Consul to be ready
+resource "null_resource" "wait-for-consul-ready" {
+  provisioner "local-exec" {
+    command = <<EOF
+for i in {1..15}; do
+  sleep $i
+  if [ $(kubectl get pod | grep -E "consul-cluster.*1/1.*Running" | wc -l) -eq 3 ]; then
+    exit 0
+  fi
+done
+
+echo "Consul pods are not ready after 2m"
+exit 1
+EOF
+  }
+
+  depends_on = ["null_resource.apply-consul"]
+}
+
+# License Consul
+resource "null_resource" "consul-license" {
+  provisioner "local-exec" {
+    command = <<EOF
+gcloud container clusters get-credentials "${google_container_cluster.vault.name}" --zone="${google_container_cluster.vault.zone}" --project="${google_container_cluster.vault.project}"
+
+kubectl exec consul-cluster-0 -- sh -c 'consul license put @/consul/license/consul.license'
+EOF
+  }
+
+  depends_on = ["null_resource.wait-for-consul-ready"]
 }
