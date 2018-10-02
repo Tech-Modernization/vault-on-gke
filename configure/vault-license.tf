73c22bf4134b45646b49da8833ba635c2b8d7538
Matt Black <github@mafro.net>
2018-10-02 13:06:54 +1000

    Install vault license

---
+++ configure/vault-license.tf
@@ -0,0 +1,14 @@
+# Vault license
+resource "null_resource" "vault-license" {
+  provisioner "local-exec" {
+    command = <<EOF
+gcloud container clusters get-credentials "${google_container_cluster.vault.name}" --zone="${google_container_cluster.vault.zone}" --project="${google_container_cluster.vault.project}"
+
+# install Vault license (which is mounted into Vault container via k8s secrets)
+kubectl exec vault-cluster-0 -- \
+  curl -X PUT \
+    -H "X-Vault-Token: ${var.vault_token}" \
+    --cacert /etc/vault/tls/ca.pem \
+    --data @/etc/vault/data/vault.license \
+    https://localhost:8200/v1/sys/license
+}
